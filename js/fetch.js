ff9234e9a204969fd0bf445d626f6586969695e7
indrariksa <indrariksa@gmail.com>
2023-03-29 13:02:49 +0700

    cek

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,7 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
+import { isiTabel } from "./table.js";
 
 let URLPresensi = "https://ws-ulbi.herokuapp.com/presensi";
 
@@ -10,5 +13,6 @@ function isiTablePresensi(results) {
 }
 
 function isiRow(value) {
-    console.log(value)
+    let content = isiTabel.replace("#NAMA#", value.Biodata.Nama).replace("#NOHP#", value.Phone_number).replace("#JABATAN#", value.Datetime).replace("#LOKASI#", value.Location).replace("#STATUS#", value.Checkin).replace("#HARIKERJA#", value.Datetime).replace("#JAMKERJA#", value.Datetime).replace("#WARNA#", getRandomColor()).replace(/#WARNALOGO#/g, getRandomColorName());
+    addInner("presensi", content);
 }
