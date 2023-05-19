94e3138be7eb275f27e7eb30585bf0274051ecac
indrariksa <indrariksa@gmail.com>
2023-05-19 13:33:07 +0700

    ubah url ketika update

--- js/config/url_put.js
+++ js/config/url_put.js
@@ -6,5 +6,5 @@ export let urlPUT = "https://ws-ulbi.herokuapp.com/upd/" + presensiId;
 export function AmbilResponse(result) {
     console.log(result); //menampilkan response API pada console
     alert(result.message); //menampilkan response API pada alert
-    window.location.reload(); //reload halaman setelah klik ok pada alert
+    window.location.href = "index.html"; //reload halaman setelah klik ok pada alert
 }
